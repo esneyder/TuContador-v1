0405abf10bbb0527d7b6a1e4383af3382d422fca
esneyder <edesalla17@gmail.com>
2015-07-28 08:35:04 -0500

    Update 2 admin module

--- admin/form-post.php
+++ admin/form-post.php
@@ -18,7 +18,7 @@
                 <div class="left_col scroll-view">
 
                     <div class="navbar nav_title" style="border: 0;">
-                        <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentellela Alela!</span></a>
+                        <a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Acirsas</span></a>
                     </div>
                     <div class="clearfix"></div>
 
